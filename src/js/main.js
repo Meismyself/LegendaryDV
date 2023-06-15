711e760c6a21cd741405c019b748989355ac3e42
zaaimioussama@gmail.com <zaaimioussama@gmail.com>
2023-06-15 04:52:59 +0100

    ghost

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+let srchBar = document.getElementById('searchBar');
+let srch = document.querySelector('#search');
+let Logo = document.querySelector('#Logo');
+let x = true;
+document.getElementById('OpSearch').addEventListener('click', function(){
+  if (x === true) {
+    srchBar.style.width = '70%';
+    srchBar.style.transition = 'all .5s ease';
+    srch.style.visibility = 'inherit';
+    Logo.style.left = '50px';
+    x = false;
+  }
+})
+this.addEventListener('scroll', function(){
+  if (x === false) {
+  srchBar.style.width = '12%';
+  srchBar.style.transition = 'all .5s ease';
+  srch.style.visibility = 'hidden';
+  Logo.style.margin = 'auto';
+  }
+})
